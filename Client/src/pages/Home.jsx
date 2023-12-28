79940e0077b4dff67ed302cf5db8a9303c8ee06d
RattanakpanhaKONG <krattanakpanha@gmail.com>
2023-12-29 01:59:06 +0700

    UI post comment and display comment immediately

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
       {listOfPosts.map((value, key) => {
         return (
           <div
+            key={key}
             id="post"
             onClick={() => {
               navigate(`/post/${value.id}`)
--- Client/src/pages/Post.jsx
+++ Client/src/pages/Post.jsx
@@ -7,10 +7,26 @@ const Post = () => {
     let  { id } = useParams()
 
     const [postObject, setPostObject] = useState({})
+    const [comments, setComments] = useState([])
+    const [newComment, setNewComment] = useState('')
+
+    const addComment = () => {
+        axios.post('http://localhost:3001/comments', { commentBody: newComment, PostId:id })
+        .then((response) => {
+          // console.log('Comment Added !')
+          const commentToAdd = { commentBody: newComment }
+          setComments([...comments, commentToAdd])
+          setNewComment("")
+        })
+    }
 
     useEffect( () => {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
           setPostObject(response.data)
+        });
+        
+        axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+          setComments(response.data)
         });
       }, []);
   
@@ -24,7 +40,29 @@ const Post = () => {
         </div>
       </div>
       <div id="rightSide" className='w-1/2'>
-        Comment Section
+        <div id="addCommentContainer" className='flex gap-x-4'>
+          <input 
+            onChange={(event) => {
+              setNewComment(event.target.value)
+            }}
+            value={newComment}
+            type="text" 
+            name="" 
+            placeholder='Comment...' 
+            className='border border-black'/>
+          <button 
+            onClick={addComment}
+            className='bg-blue-500 rounded-lg text-white p-2'>Add Comment</button>
+        </div>
+        <div id="listOfComments" className=''>
+          {comments.map((comment, key) => {
+            return (
+            <div key={key} className='border border-black my-2'> 
+              <p>{comment.commentBody}</p> 
+            </div>
+            
+            )})}
+        </div>
       </div>
     </div>
   )
